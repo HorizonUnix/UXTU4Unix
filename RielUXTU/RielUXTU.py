c845e0df17c5aeed54426f4739d6d7a6fa4a07aa
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2024-01-30 16:20:38 +0700

    Add files via upload

--- RielUXTU/RielUXTU.py
+++ RielUXTU/RielUXTU.py
@@ -2,6 +2,8 @@ import subprocess
 import sys
 import time
 import os
+import urllib.request
+from configparser import ConfigParser
 
 PRESETS = {
     "Eco": (
@@ -62,36 +64,83 @@ PRESETS = {
     )
 }
 
+def create_config() -> None:
+    cfg: ConfigParser = ConfigParser()
+    cfg.add_section('User')
+    print("------ First time setup ------")
+    print("Preset power plan")
+    for i, mode in enumerate(PRESETS, start=1):
+        print(f"{i}. {mode}")
+
+    choice = input("Choose your preset power plan by pressing number: ")
+    try:
+        preset_number = int(choice)
+        preset_name = list(PRESETS.keys())[preset_number - 1]
+        cfg.set('User', 'Mode', preset_name)
+        with open('config.ini', 'w') as config_file:
+            cfg.write(config_file)
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
+def read_config() -> str:
+    cfg: ConfigParser = ConfigParser()
+    cfg.read('config.ini')
+    return cfg.get('User', 'Mode', fallback='')
+
+def check_config_integrity(conf: ConfigParser) -> None:
+    config_path = 'config.ini'
+    
+    if not os.path.isfile(config_path) or os.stat(config_path).st_size == 0:
+        create_config()
+        return
+
+    conf.read(config_path)
+    
+    if not conf.has_section('User'):
+        create_config()
+
+def get_latest_version():
+    latest_version_url = "https://github.com/gorouflex/rieluxtu4mac/releases/latest"
+    latest_version = urllib.request.urlopen(latest_version_url).geturl()
+    return latest_version.split("/")[-1]
+
+def check_for_updates():
+    local_version = "0.0.4"
+    latest_version = get_latest_version()
+
+    if local_version < latest_version:
+        print("A new update is available! Please visit the following link for details:")
+        print("https://github.com/gorouflex/RielUXTU4Mac/releases/latest")
+        sys.exit()
+    elif local_version > latest_version:
+        print("Welcome to RielUXTU4Mac Beta Program.")
+        print("This build may not be as stable as expected. Only for testing purposes!")
+        result = input("Do you want to continue? (y/n): ").lower()
+        if result != "y":
+            sys.exit()
+        
 def clear():
     _ = os.system('cls') if os.name == 'nt' else os.system('clear')
-    
+
 def run_command(args):
     command = ["sudo", "./ryzenadj"] + args.split()
     while True:
         subprocess.run(command)
         time.sleep(3)
-        print("Script will reapplied every 3 seconds since RyzenAdj can easily reset just like UXTU")
         clear()
+        print("Script will be reapplied every 3 seconds since RyzenAdj can easily reset just like UXTU")
 
 
-if len(sys.argv) > 1:
-    user_input = sys.argv[1]
-    try:
-        preset_number = int(user_input)
-        preset_name = list(PRESETS.keys())[preset_number - 1]
-        run_command(PRESETS[preset_name])
-    except ValueError:
-        run_command(user_input)
+check_for_updates()
+
+config = ConfigParser()
+check_config_integrity(config)
+config.read('config.ini')
+
+user_mode = read_config()
+
+if user_mode:
+    print(f"Using mode: {user_mode}")
+    run_command(PRESETS[user_mode])
 else:
-    print("Current Preset:")
-    for i, preset in enumerate(PRESETS, start=1):
-        print(f"{i}. {preset}")
-    user_input = input("Choose a preset by enter number or enter custom arguments: ")
-    print("Script will reapplied every 3 seconds since RyzenAdj can easily reset just like UXTU")
-
-    try:
-        preset_number = int(user_input)
-        preset_name = list(PRESETS.keys())[preset_number - 1]
-        run_command(PRESETS[preset_name])
-    except ValueError:
-        run_command(user_input)
+    print("Config file is missing or invalid. Please run the script again.")
