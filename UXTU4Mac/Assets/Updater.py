65f96adf73489bd83225697763c0e7db8a4d5af7
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2024-02-23 23:42:17 +0700

    fix for chmod

--- UXTU4Mac/Assets/Updater.py
+++ UXTU4Mac/Assets/Updater.py
@@ -2,6 +2,7 @@ import os
 import urllib.request
 import zipfile
 import shutil
+import subprocess
 
 def update():
     url = "https://github.com/AppleOSX/UXTU4Mac/releases/latest/download/UXTU4Mac.zip"
@@ -13,17 +14,14 @@ def update():
     with zipfile.ZipFile(os.path.join(current_dir, "UXTU4Mac.zip"), 'r') as zip_ref:
         zip_ref.extractall(new_folder)
     
-    uxtu_command_path = os.path.join(current_folder, "UXTU4Mac.command")
-    os.chmod(uxtu_command_path, 0o755)
-    
-    assets_folder = os.path.join(new_folder, "Assets")
-    ryzenadj_path = os.path.join(assets_folder, "ryzenadj")
-    os.chmod(ryzenadj_path, 0o755)
-    
     shutil.rmtree(current_folder)
     inner_folder = os.path.join(new_folder, "UXTU4Mac")
     shutil.move(inner_folder, current_dir)
     shutil.rmtree(new_folder)
+
+    # Add executable permissions to the files after the update
+    subprocess.call(['chmod', '+x', os.path.join(current_dir, "UXTU4Mac", "UXTU4Mac.command")])
+    subprocess.call(['chmod', '+x', os.path.join(current_dir, "UXTU4Mac", "Assets", "ryzenadj")])
 
 if __name__ == "__main__":
     update()
