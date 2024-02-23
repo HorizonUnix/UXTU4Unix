44c50cd34eb799f0ce1dbe78a53a1a9c2566d9ff
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2024-02-23 23:28:55 +0700

    support chmod +x

--- UXTU4Mac/Assets/Updater.py
+++ UXTU4Mac/Assets/Updater.py
@@ -12,6 +12,14 @@ def update():
     urllib.request.urlretrieve(url, os.path.join(current_dir, "UXTU4Mac.zip"))
     with zipfile.ZipFile(os.path.join(current_dir, "UXTU4Mac.zip"), 'r') as zip_ref:
         zip_ref.extractall(new_folder)
+    
+    uxtu_command_path = os.path.join(current_folder, "UXTU4Mac.command")
+    os.chmod(uxtu_command_path, 0o755)
+    
+    assets_folder = os.path.join(new_folder, "Assets")
+    ryzenadj_path = os.path.join(assets_folder, "ryzenadj")
+    os.chmod(ryzenadj_path, 0o755)
+    
     shutil.rmtree(current_folder)
     inner_folder = os.path.join(new_folder, "UXTU4Mac")
     shutil.move(inner_folder, current_dir)
