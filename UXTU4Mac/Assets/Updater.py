5904918e89242d394585b1c9bbbc5d6bad367f78
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2024-02-25 19:43:53 +0700

    fix when after update doesn't delete file zip

--- UXTU4Mac/Assets/Updater.py
+++ UXTU4Mac/Assets/Updater.py
@@ -1,9 +1,5 @@
-import os
-import urllib.request
-import zipfile
-import shutil
-import subprocess
-
+import os, urllib.request, zipfile, shutil, subprocess
+ 
 def update():
     url = "https://github.com/AppleOSX/UXTU4Mac/releases/latest/download/UXTU4Mac.zip"
     script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -14,14 +10,15 @@ def update():
     backup_config = os.path.join(current_dir, "config.ini.bak")
     logs_folder = os.path.join(current_folder, "Logs")
     backup_logs = os.path.join(current_dir, "Logs.bak")
+    zip_file_path = os.path.join(current_dir, "UXTU4Mac.zip")
 
     if os.path.exists(config_file):
         shutil.copy2(config_file, backup_config)
     if os.path.exists(logs_folder):
         shutil.copytree(logs_folder, backup_logs)
 
-    urllib.request.urlretrieve(url, os.path.join(current_dir, "UXTU4Mac.zip"))
-    with zipfile.ZipFile(os.path.join(current_dir, "UXTU4Mac.zip"), 'r') as zip_ref:
+    urllib.request.urlretrieve(url, zip_file_path)
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(new_folder)
     
     shutil.rmtree(current_folder)
@@ -36,6 +33,9 @@ def update():
         shutil.move(backup_config, config_file)
     if os.path.exists(backup_logs):
         shutil.move(backup_logs, logs_folder)
+
+    if os.path.exists(zip_file_path):
+        os.remove(zip_file_path)
 
 if __name__ == "__main__":
     update()
